57f5cf50d458b2dbe0dfc8e292391ec2a441f5b5
HIMANSHU SHUKLA <112720998+Himanshu-su@users.noreply.github.com>
2022-12-17 22:42:48 +0530

    Add files via upload

--- admin_register.js
+++ admin_register.js
@@ -1,27 +1,42 @@
 
-document.getElementById('goggleSign').addEventListener('click', goggleWeb)
+// document.getElementById('goggleSign').addEventListener('click', goggleWeb)
 
-function goggleWeb() {
-    window.location.href = 'https://www.google.com/'
-}
+// function goggleWeb() {
+//     window.location.href = 'https://www.google.com/'
+
+//     document.getElementById('RegisteredBtn').addEventListener('click', valid)
 
 
-document.getElementById('ancherRegister').addEventListener('click', loginPage)
 
-function loginPage() {
-    window.location.href = 'login.html'
-}
 
 document.getElementById('RegisteredBtn').addEventListener('click', valid)
 
 
 
-function valid(event) {
-    event.preventDefault()
+function valid() {
 
-    let email = document.getElementById('email').value;
 
+    let email = document.getElementById('emailId').value;
+    let password = document.getElementById('passwordId').value;
+
+
+    // if (email === '' || null) {
+    //     window.alert('please enter mail id');
+    // }
+    // else if (password === '' || null) {
+    //     window.alert('please enter your password');
+    // }
+    // else {
+    //     window.alert('Thanks for sign in');
+    // }
     localStorage.setItem('email1', email)
+    // localStorage.setItem('ConfirmEmai1', JSON.stringify(ConfirmEmail))
+    localStorage.setItem('password1', password)
+    // localStorage.setItem('ConfirmPassword1', JSON.stringify(ConfirmPassword))
+    // localStorage.setItem('CellPhone1', JSON.stringify(CellPhone))
+
+
+
 
 
 }
